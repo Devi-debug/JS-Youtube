85240ea9a1d4c5743a9a2a5f10d00c6aeee80ffa
Devi-debug <itsomm143@gmail.com>
2024-11-01 20:56:46 +0530

    Datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive data type
+
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const   scoreValue = 100.58
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+
+const bigNumber = 546546165564
+
+// Refference type or Non-primitive type
+
+// Array, objects, Functions
+
+const heros = ["shaktiman", "naagraj","Doga"];
+let myObj= {
+    name:"Devi"
+    age:18
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
