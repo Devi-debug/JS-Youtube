77a92adb6a704281a979a0ac46407307f8836404
Devi-debug <itsomm143@gmail.com>
2024-11-07 08:22:46 +0530

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+
+const balance =new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.5689
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-US'));
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(1,6,9,4,7,1,3,4,5));
+// console.log(Math.max(4,8,9));
+
+// console.log(Math.random())
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+min =10
+max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
