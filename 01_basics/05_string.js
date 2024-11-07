e5957a98ce1e84639c226240595d451a8da299e7
Devi-debug <itsomm143@gmail.com>
2024-11-07 07:43:18 +0530

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+const name="Devi"
+const repoCount = 5
+
+// console.log(name + repoCount + " value"); Outdated
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName =new String(`Deviprasanna-sh-com`);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(1));
+// console.log(gameName.indexOf('e'));
+
+// const newString = gameName.substring(0,6)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-11,9)
+// console.log(anotherString);
+
+
+// const newStringOne = "     Devi     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url ="https://courses.chaicode.com/learn/home/chai-aur-javascript-in-hindi/section/493398/lesson/3060445?"
+console.log(url.replace('/','['));
+console.log(url.includes('Devi'));
+
+console.log(gameName.split('-'));
